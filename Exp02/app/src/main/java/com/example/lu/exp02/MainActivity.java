9deaceb4aa927ef49e604e08d92102602161dd32
lu <957884803@qq.com>
2016-10-12 23:32:21 +0800

    完成主界面

---
+++ Exp02/app/src/main/java/com/example/lu/exp02/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lu.exp02;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
